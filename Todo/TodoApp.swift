0986a17969c9bf1876d6ac9093be0c333d558345
geraldaddey <geraldaddey@yahoo.com>
2022-09-14 11:09:22 +0000

    Initial Commit

---
+++ Todo/TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoApp.swift
+//  Todo
+//
+//  Created by Quansah on 14/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
